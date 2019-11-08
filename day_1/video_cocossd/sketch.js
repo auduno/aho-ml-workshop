b99a656c73f2cdacda99e8375387c7ba3bfd06d1
Audun Mathias Øygard <amoygard@gmail.com>
2019-11-08 23:40:45 +0100

    cleanup + add to readme

--- day_1/video_cocossd/sketch.js
+++ day_1/video_cocossd/sketch.js
@@ -2,71 +2,43 @@ let video;
 let status;
 let detectionModel;
 let objects = [];
-let trigger = true;
-let videoPromise;
-let modelPromise;
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.elt.addEventListener('loadedmetadata', loadCocoModel);
-  /*videoPromise = new Promise(resolve => {
-            video.elt.onloadedmetadata = () => {
-              resolve();
-            };
-          });*/
-
-  // Load CocoSSD model
-  /*modelPromise = cocoSsd.load({
-    'base': 'mobilenet_v2'
-  });*/
-  
-  /*Promise.all([modelPromise, videoPromise])
-        .then(values => {
-          detectionModel = values[0];
-          //this.detectFromVideoFrame(values[0], this.videoRef.current);
-          detect();
-        });*/
-    
-  cocoSsd.load({
-    'base': 'mobilenet_v2'
-  }).then(modelReady);
-
-  // Hide the original video
   video.hide();
   status = select('#status');
-}
-
-function loadCocoModel() {
-  // Load CocoSSD model
-  if (trigger) { 
-    trigger = false;
-    cocoSsd.load({
-      'base': 'mobilenet_v2'
-    }).then(modelReady);
-  }
 }
 
 function draw() {
   image(video, 0, 0, width, height);
   for (let i = 0; i < objects.length; i++) {
+    let objectX = objects[i].bbox[0];
+    let objectY = objects[i].bbox[1];
+    let objectWidth = objects[i].bbox[2];
+    let objectHeight = objects[i].bbox[3];
+    let objectClass = objects[i].class;
+    let objectScore = objects[i].score;
     noStroke();
-    fill(0, 255, 0);
-    text(objects[i].class, objects[i].bbox[0], objects[i].bbox[1]);
+    fill(0, 0, 0);
+    text(objectClass, objectX, objectY - 5);
     noFill();
-    strokeWeight(4);
+    strokeWeight(2);
     stroke(0, 255, 0);
-    rect(objects[i].bbox[0], objects[i].bbox[1], objects[i].bbox[2], objects[i].bbox[3]);
+    rect(objectX, objectY, objectWidth, objectHeight);
   }
+}
+
+function loadCocoModel() {
+  cocoSsd.load({
+    'base': 'mobilenet_v2'
+  }).then(modelReady);
 }
 
 function modelReady(model) {
   detectionModel = model;
-  startDetecting();
-}
-
-function startDetecting() {
   status.html('Model loaded!');
   detect();
 }
