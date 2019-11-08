f8f285d8ab15af7723d229ea7bf2771a2d672068
Audun Mathias Øygard <amoygard@gmail.com>
2019-11-09 00:40:13 +0100

    Change posenet to p5js format

---
+++ day_1/video_posenet/sketch.js
@@ -0,0 +1,66 @@
+let video;
+let poseNet;
+let poses = [];
+let status;
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  poseNet = ml5.poseNet(video, modelReady);
+  // trigger detectedPose on pose events
+  poseNet.on('pose', detectedPose);
+
+  video.hide();
+  status = select('#status');
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+  // draw detected points (if any)
+  drawKeypoints();
+  drawSkeleton();
+}
+
+function modelReady() {
+  status.html('Model Loaded');
+}
+
+function detectedPose(results) {
+  poses = results;
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse if the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(0, 255, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(0, 255, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
